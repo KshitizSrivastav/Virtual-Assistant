d8ba91d94eeaea887800d583be385af3b3c73924
KshitizSr <kshitiz@bitloka.com>
2026-02-19 21:14:15 +0530

    Add Cloudinary and multer along with UserContext

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ app.use(cors({
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
-app.use(cookieParser());
 
 app.listen(port, () => {
     connectDb();
